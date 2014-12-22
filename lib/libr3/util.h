924cd04e532b5672300fe6f1100a04f3ef6d333c
Michael T. Reece <mtreece@ncsu.edu>
2014-12-22 16:17:55 -0500

    TBS add util.h

---
+++ lib/libr3/util.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2014 Michael T. Reece.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#include <config.h>
+
+#endif /* _UTIL_H_ */
+// vim: et ts=4 sw=4 sts=-1 :
