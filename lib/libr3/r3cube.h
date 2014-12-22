d6d6578fadbcf023c801be1ca7d25c48fa3b95d6
Michael T. Reece <mtreece@ncsu.edu>
2014-12-22 00:31:26 -0500

    add copyright

--- lib/libr3/r3cube.h
+++ lib/libr3/r3cube.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2014 Michael T. Reece.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
 #ifndef _R3CUBE_H_
 #define _R3CUBE_H_
 
--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2014 Michael T. Reece.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
 #include <r3.h>
 
 int main(int argc, char *argv[])
