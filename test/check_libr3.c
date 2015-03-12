7f1b34fc2fd87f7e25962072ad1cffa26d1e7690
Michael T. Reece <mtreece@ncsu.edu>
2015-03-12 01:57:46 -0400

    add basic framework for make-check

---
+++ test/check_libr3.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2015 Michael T. Reece.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#include <config.h>
+
+#include <stdlib.h>
+
+#include <check.h>
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
+
+// vim: et ts=4 sw=4 :
