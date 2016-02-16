386595d11dbc70ba518f0051d0540dc027e99148
Michael T. Reece <mtreece@ncsu.edu>
2016-02-15 21:40:56 -0500

    generalize to specify which side to pull from

--- lib/libr3/r3_get_cell.h
+++ lib/libr3/r3_get_cell.h
@@ -6,8 +6,8 @@
  * published by the Free Software Foundation.
  *
  */
-#ifndef _R3_GET_FACING_CELL_
-#define _R3_GET_FACING_CELL_
+#ifndef _R3_GET_CELL_
+#define _R3_GET_CELL_
 
 #include <config.h>
 
@@ -16,16 +16,20 @@
 
 /**
  * @brief
- * From the facing r3side of this r3cube, get the r3cell that is located at
- * specified row and column.
+ * From the specified r3side of this r3cube, get the r3cell that is located at
+ * the specified row and column.
+ *
+ * The number `side` should adhere to the numbering scheme specified in
+ * /doc/cube-layout.txt. (e.g., the facing side will be on side = 0).
  *
  * @param cube The r3cube to operate on.
- * @param row The row of the facing side to pull from.
- * @param col The column of the facing side to pull from.
+ * @param side The side-number of the side to return a cell from.
+ * @param row The row of the side to pull from.
+ * @param col The column of the side to pull from.
  *
- * @return The facing cell at the specified row, col; NULL on error.
+ * @return The cell at the specified row, col, side; NULL on error.
  */
-const r3cell *r3_get_facing_cell(r3cube *cube, unsigned row, unsigned col);
+const r3cell *r3_get_cell(r3cube *cube, unsigned side, unsigned row, unsigned col);
 
-#endif /* _R3_GET_FACING_CELL_ */
+#endif /* _R3_GET_CELL_ */
 // vim: et ts=4 sw=4 :
