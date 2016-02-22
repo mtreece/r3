700fd381e0d33745397da06a613fe8d327aa4817
Michael T. Reece <mtreece@ncsu.edu>
2016-02-21 19:13:26 -0500

    create prng_move test to exercise lots of r3_move moves

--- test/check_libr3-core.c
+++ test/check_libr3-core.c
@@ -10,6 +10,7 @@
 #include <config.h>
 
 #include <stdlib.h>
+#include <time.h>
 
 #include <r3.h>
 #include <r3_synclinks.h>
@@ -70,6 +71,39 @@ START_TEST(test_basics)
 }
 END_TEST
 
+/**
+ * @brief
+ * Test that we can move in many, many different ways without failing.
+ *
+ * @details
+ * This test issues r3_move calls from a const-seeded prng so as to exercise
+ * lots of "arbitrary" calls. The need for this test arose when I was trying to
+ * refactor parts of my core algorithm, using the test-suite to ensure I hadn't
+ * broken anything: I found that simply issuing r3_move against each direction
+ * and selector wasn't thorough enough of a test in some refactoring attempts.
+ */
+START_TEST(prng_move)
+{
+    const int directions[] = {
+        R3_UP,
+        R3_DOWN,
+        R3_LEFT,
+        R3_RIGHT,
+    };
+    r3cube cube;
+
+    ck_assert_int_eq(0, r3_init(&cube));
+
+    srand(0);
+
+    for (unsigned i = 0; i < 1024; ++i) {
+        unsigned direction = directions[rand() % sizeof(directions)/sizeof(directions[0])];
+        unsigned selector = rand() % MAX_ROW_COLS; // TODO: accommodate for nrows != ncols
+        ck_assert_int_eq(0, r3_move(&cube, direction, selector));
+    }
+}
+END_TEST
+
 static Suite *builder()
 {
     Suite *s;
@@ -83,6 +117,7 @@ static Suite *builder()
 
     tc = tcase_create("basics");
     tcase_add_test(tc, test_basics);
+    tcase_add_test(tc, prng_move);
     suite_add_tcase(s, tc);
 
     return s;
