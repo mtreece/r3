57dfa34b9beb3fd25e2d0f062b4c9b2a9c3069da
Michael T. Reece <mtreece@ncsu.edu>
2016-03-31 08:27:49 -0400

    update dummy main to handle r3_init API change

--- src/main.c
+++ src/main.c
@@ -43,35 +43,40 @@
 
 int main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
 {
-    r3cube cube;
+    r3cube *cube;
+    size_t cubelen = 0;
     char buffer[R3_SPRINT_MINLENGTH];
     size_t buflen;
 
-    DO_CHECK_REPORT_END(0 !=, r3_init(&cube));
+    DO_CHECK_REPORT_END(-2 !=, r3_init(NULL, &cubelen));
+    printf("r3cube length reported to be %zu bytes\n", cubelen);
+
+    cube = malloc(cubelen);
+    DO_CHECK_REPORT_END(0 !=, r3_init(cube, NULL));
 
     buflen = sizeof(buffer);
-    DO_CHECK_REPORT_END(0 !=, r3_sprint(buffer, &buflen, &cube));
+    DO_CHECK_REPORT_END(0 !=, r3_sprint(buffer, &buflen, cube));
     printf("Identity:\n");
     printf("%s\n", buffer);
     printf("==========================================\n");
 
-    DO_MOVE_AND_PRINT(&cube, R3_UP, 0);
-    DO_MOVE_AND_PRINT(&cube, R3_UP, 0);
-    DO_MOVE_AND_PRINT(&cube, R3_UP, 0);
+    DO_MOVE_AND_PRINT(cube, R3_UP, 0);
+    DO_MOVE_AND_PRINT(cube, R3_UP, 0);
+    DO_MOVE_AND_PRINT(cube, R3_UP, 0);
 
-    DO_MOVE_AND_PRINT(&cube, R3_UP, 1);
-    DO_MOVE_AND_PRINT(&cube, R3_UP, 1);
-    DO_MOVE_AND_PRINT(&cube, R3_UP, 1);
+    DO_MOVE_AND_PRINT(cube, R3_UP, 1);
+    DO_MOVE_AND_PRINT(cube, R3_UP, 1);
+    DO_MOVE_AND_PRINT(cube, R3_UP, 1);
 
-    DO_MOVE_AND_PRINT(&cube, R3_UP, 2);
-    DO_MOVE_AND_PRINT(&cube, R3_UP, 2);
-    DO_MOVE_AND_PRINT(&cube, R3_UP, 2);
+    DO_MOVE_AND_PRINT(cube, R3_UP, 2);
+    DO_MOVE_AND_PRINT(cube, R3_UP, 2);
+    DO_MOVE_AND_PRINT(cube, R3_UP, 2);
 
-    DO_MOVE_AND_PRINT(&cube, R3_DOWN, 0);
-    DO_MOVE_AND_PRINT(&cube, R3_DOWN, 0);
-    DO_MOVE_AND_PRINT(&cube, R3_DOWN, 0);
+    DO_MOVE_AND_PRINT(cube, R3_DOWN, 0);
+    DO_MOVE_AND_PRINT(cube, R3_DOWN, 0);
+    DO_MOVE_AND_PRINT(cube, R3_DOWN, 0);
 
-    DO_MOVE_AND_PRINT(&cube, R3_RIGHT, 0);
+    DO_MOVE_AND_PRINT(cube, R3_RIGHT, 0);
 
     return 0;
 }
